edee80cfde05d4866bb86cb76e4d5cc86c6e1fd7
Landaverde-04 <landaverde.kevin04@gmail.com>
2026-08-30 14:38:36 -0600

    feat: vista previa visual del modulo de pacientes 2

--- sistema_prosalud/urls.py
+++ sistema_prosalud/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('seguridad/', include('seguridad.urls')),
+    path('pacientes/', include('pacientes.urls')),
 ]
