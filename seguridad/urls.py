56cb7dde78aaf632ec1eadf669b592c940322bdf
Landaverde-04 <landaverde.kevin04@gmail.com>
2026-08-30 14:24:59 -0600

    feat: cierre de sesion por inactividad con aviso al usuario

--- seguridad/urls.py
+++ seguridad/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'seguridad'
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='seguridad/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', views.IniciarSesionView.as_view(), name='login'),
+    path('logout/', views.CerrarSesionView.as_view(), name='logout'),
 
     # Usuarios
     path('usuarios/', views.lista_usuarios, name='lista_usuarios'),
